9ae97baf983e5e038375fc41e089f096c60b3b00
boierul <theboierul@gmail.com>
2023-05-11 20:11:41 +0200

    ..

---
+++ project-code/best-movies/src/app/home/page.js
@@ -0,0 +1,24 @@
+'use client';
+import React, {useEffect, useState} from 'react';
+
+function Page() {
+    // const [state, setState] = useState();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // useEffect(async () => {
+    //     const result = await fetch('https://api.themoviedb.org/3/movie/550?api_key=1f54bd990f1cdfb230adb312546d765d')
+    //         .then(setState(await result.json()));
+    //
+    //     console.log(await result.json())
+    // }, []);
+
+
+    return (
+        <div>
+            a
+            {/*{state.id}*/}
+        </div>
+    );
+}
+
+export default Page;
