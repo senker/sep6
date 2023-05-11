7a198715f714edd702c5f1dfdc57aa8c384e1cfd
boierul <theboierul@gmail.com>
2023-05-11 20:45:48 +0200

    Implemented first iteration of folder structure

--- project-code/best-movies/src/app/page.js
+++ project-code/best-movies/src/app/page.js
@@ -1,14 +1,37 @@
+'use client';
+import React, {useEffect, useState} from "react";
 import styles from './page.module.css'
 
 export default function Home() {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    // useEffect(async () => {
-    //     const result = await fetch('https://api.themoviedb.org/3/movie/550?api_key=1f54bd990f1cdfb230adb312546d765d');
-    // }, []);
+    const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch('https://api.themoviedb.org/3/movie/550?api_key=1f54bd990f1cdfb230adb312546d765d')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+                setLoading(false);
+                console.log(data)
+            });
+    }, []);
+
+    if (isLoading) return <p>Loading...</p>;
+    if (!data) return <p>No profile data</p>;
 
   return (
-    <main>
-      heyo
-    </main>
+      <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+      }}>
+          <h1>Title: {data.title}</h1>
+          <h1>ID: {data.id}</h1>
+          <h1>Status: {data.status}</h1>
+          <h1>Tagline: {data.tagline}</h1>
+      </div>
   )
 }
---
+++ project-code/best-movies/src/components/demo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Demo() {
+    return (
+        <div>lll</div>
+    );
+}
+
+export default Demo;
