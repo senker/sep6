c7a5fcde62e578d06a4e98efd07d3fcc2d470ad9
boierul <theboierul@gmail.com>
2023-05-11 20:16:31 +0200

    ..

--- project-code/best-movies/src/app/home/page.js
+++ project-code/best-movies/src/app/home/page.js
@@ -2,21 +2,27 @@
 import React, {useEffect, useState} from 'react';
 
 function Page() {
-    // const [state, setState] = useState();
+    const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(false);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    // useEffect(async () => {
-    //     const result = await fetch('https://api.themoviedb.org/3/movie/550?api_key=1f54bd990f1cdfb230adb312546d765d')
-    //         .then(setState(await result.json()));
-    //
-    //     console.log(await result.json())
-    // }, []);
+    useEffect(() => {
+        setLoading(true);
+        fetch('https://api.themoviedb.org/3/movie/550?api_key=1f54bd990f1cdfb230adb312546d765d')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+                console.log(data)
+                setLoading(false);
+            });
+    }, []);
+
+    if (isLoading) return <p>Loading...</p>;
+    if (!data) return <p>No profile data</p>;
 
 
     return (
         <div>
-            a
-            {/*{state.id}*/}
+            <h1>{data.title}</h1>
         </div>
     );
 }
