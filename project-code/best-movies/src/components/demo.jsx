4ff77385185f7fc97c9e85971250403c30d60615
senker <senkerplov@gmail.com>
2023-05-14 23:45:57 +0200

    🚧 refactor: convert project to typescript

--- project-code/best-movies/src/components/demo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Demo() {
-    return (
-        <div>lll</div>
-    );
-}
-
-export default Demo;
--- project-code/best-movies/src/utils/demo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Demo(props) {
-    return (
-        <div></div>
-    );
-}
-
-export default Demo;
