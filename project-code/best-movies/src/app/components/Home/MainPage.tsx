c6a345fe72861d628edcb794c4a89fa3c26422c6
senker <senkerplov@gmail.com>
2023-05-15 16:42:33 +0200

    ❇️ feat: Prisma ORM & Railway db connectivity
    Creating one demo user, dividing server and client components

---
+++ project-code/best-movies/src/app/components/Home/MainPage.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Movie } from "@/types/movie.dto";
+import styles from "../../page.module.css";
+
+
+const MainPage:React.FC = () =>  {
+
+const TRENDING_MOVIES_URL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
+
+  const [trendingMovies, setTrendingMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    fetch(TRENDING_MOVIES_URL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const trendingMovies = data.results;
+        setTrendingMovies(trendingMovies);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className={styles.body}>
+      <h1>Trending Movies This Week</h1>
+      <ul>
+        {trendingMovies.map((movie) => (
+          <li key={movie.id}>
+            {movie.title} - {movie.release_date.toString()}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MainPage;
