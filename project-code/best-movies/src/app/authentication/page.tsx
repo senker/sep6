4fb486320a0b8c5848d417d9f920b95fc2a79a2e
senker <senkerplov@gmail.com>
2023-05-17 10:58:58 +0200

    ❇️ add: auth, jwt session update + new data

--- project-code/best-movies/src/app/authentication/page.tsx
+++ project-code/best-movies/src/app/authentication/page.tsx
@@ -1,12 +1,22 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { LoginButton, LogoutButton } from "../auth";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
   return (
     <div>
-        <h1>Login page.</h1>
-        <h1>Login page.</h1>
-        <h1>Login page.</h1>
-        <h1>Login page.</h1>
-        <h1>Login page.</h1>
+      <div>  ---  </div>
+      <h1>Login page.</h1>
+
+      <LoginButton />
+      <LogoutButton />
+
+      return (
+      <main>
+        <pre>{JSON.stringify(session)}</pre>
+      </main>
+      );
     </div>
-    )
+  );
 }
