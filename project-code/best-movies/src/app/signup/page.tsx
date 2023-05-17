5780bb785e245d07334dca6d513d5756933ad418
senker <senkerplov@gmail.com>
2023-05-17 14:05:23 +0200

    Update page.tsx

--- project-code/best-movies/src/app/signup/page.tsx
+++ project-code/best-movies/src/app/signup/page.tsx
@@ -14,11 +14,38 @@ const SignUpForm = dynamic(() => import("../components/SignUp/SignUpForm"), {
   ssr: false,
 });
 
+async function createUser(formData: SignUpFormData) {
+    try {
+      const response = await fetch("/api/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      if (response.ok) {
+        // User created successfully
+        const user = await response.json();
+        console.log("User created:", user);
+      } else {
+        // Error creating user
+        const error = await response.text();
+        console.error("Error creating user:", error);
+      }
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+  }
+
 export default function SignUpPage() {
   const handleSignUp = (formData: SignUpFormData) => {
     // Access the form data here
+    createUser(formData);
     console.log(formData);
   };
+
+  
 
   return (
     <>
