bb5d03a5d90726b204240ff6c31da941b6cf6265
boierul <theboierul@gmail.com>
2023-05-22 21:27:51 +0200

    ...

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,54 +2,55 @@
 import Link from "next/link";
 import styles from "./Header.module.scss"
 import SearchBar from "../SearchBar/SearchBar";
-import { Movie } from "@/types/movie.dto";
+import {Movie} from "@/types/movie.dto";
 import {useEffect, useState} from "react";
 
 interface HeaderProps {
-  onSearch: (searchTerm: string) => void;
-  initialMovies: Movie[];
+    onSearch: (searchTerm: string) => void;
+    initialMovies: Movie[];
 }
 
-const Header: React.FC<HeaderProps> = ({ onSearch }) => {
-  console.log("onSearch prop:", onSearch);
+const Header: React.FC<HeaderProps> = ({onSearch}) => {
+    console.log("onSearch prop:", onSearch);
 
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = event.target.value;
-    onSearch(searchTerm);
-  };
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const searchTerm = event.target.value;
+        onSearch(searchTerm);
+    };
 
-  // const [isScrolled, setIsScrolled] = useState(false);
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     if (window.scrollY > 0) {
-  //       setIsScrolled(true);
-  //     } else {
-  //       setIsScrolled(false);
-  //     }
-  //   };
-  //
-  //   window.addEventListener("scroll", handleScroll);
-  //
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
+    /* ------------------------------ SCROLLING EFFECT ------------------------------ */
+    const [isScrolled, setIsScrolled] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
 
-  return (
-    <header className={styles.header}>
-      <nav className={styles.navbar}>
-        <Link href="/" className={styles.logo}>Best Movies</Link>
+        window.addEventListener("scroll", handleScroll);
 
-        <div className={styles.container}>
-          <SearchBar 
-            placeholder="Search for movies"
-            onChangeHandler={(e) => handleSearch(e)}
-          />
-          <Link href="/authentication" className={styles.link}>Login</Link>
-        </div>  
-      </nav>
-    </header>
-  );
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <header className={`${styles.header} ${isScrolled ? styles.scrolled : styles.notScrolled}`}>
+            <nav className={styles.navbar}>
+                <Link href="/" className={styles.logo}>Best Movies</Link>
+
+                <div className={styles.container}>
+                    <SearchBar
+                        placeholder="Search for movies"
+                        onChangeHandler={(e) => handleSearch(e)}
+                    />
+                    <Link href="/authentication" className={styles.link}>Login</Link>
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
