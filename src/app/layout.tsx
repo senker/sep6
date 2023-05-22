d2fa37261bf184ccdcc3d4a2384da805bc046bdc
andronachi09 <tudor.andronachi09@gmail.com>
2023-05-22 21:07:33 +0200

    fix code for build-and-deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,10 @@ import Header from "./components/Header/Header";
 import { Providers } from "./providers";
 import styles from "./page.module.scss"
 
-export const metadata = {
-  title: "Best Movies | Home",
-  description: "Eyes flickin` good",
-};
+// export const metadata = {
+//   title: "Best Movies | Home",
+//   description: "Eyes flickin` good",
+// };
 
 export default function RootLayout({
   children,
