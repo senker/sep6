fab212e90927dbca3a507c6c1776c43adb81999f
senker <senkerplov@gmail.com>
2023-05-24 14:41:50 +0200

    ❇️ add: favorites feature part impl
    Co-Authored-By: Dan Pintea <49337867+Boierul@users.noreply.github.com>
    Co-Authored-By: Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -15,7 +15,7 @@ export default async function HomePage() {
 
       return (
       <main>
-        <pre>{JSON.stringify(session)}</pre>
+        <pre>{JSON.stringify(session?.user?.email)}</pre>
       </main>
       );
     </div>
