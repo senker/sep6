99bed7e52c2a769236a9b7a6d403752712653e91
senker <senkerplov@gmail.com>
2023-05-24 23:51:36 +0200

    ❇️ add: custom session hook w/ favourites

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,34 +1,36 @@
-'use client';
-import { useSession } from "next-auth/react";
+"use client";
 import { useEffect, useState } from "react";
 import Row from "../components/Row/Row";
 import styles from "./Dashboard.module.scss";
 import { Movie } from "@/types/movieFull.dto";
-
+import { useCustomSession } from "../../hooks/useCustomSession";
 
 export default function Dashboard() {
-  const { data: session } = useSession();
+  const { data: session } = useCustomSession();
   const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
     async function fetchMovies() {
-      // Check if the session exists and has the favourites array
-      if (session && session.user.favourites && session.user.favourites.length > 0) {
-        const moviePromises = session.user.favourites.map(async (movieId: any) => {
-          const response = await fetch(
-            `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&append_to_response=videos`
-          );
-          return response.json();
-        });
+      if (
+        session &&
+        session.user?.favourites &&
+        session.user?.favourites.length > 0
+      ) {
+        const moviePromises = session.user?.favourites.map(
+          async (movieId: any) => {
+            const response = await fetch(
+              `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&append_to_response=videos`
+            );
+            return response.json();
+          }
+        );
         const fetchedMovies = await Promise.all(moviePromises);
         setMovies(fetchedMovies);
-  console.log(movies);
-
+        console.log(movies);
       }
     }
     fetchMovies();
   }, [session]);
-
 
   return (
     <div className={styles.body}>
